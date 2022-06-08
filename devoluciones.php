848cde4dc095a29726744b678b6a10f8f8226708
Douglas Hernandez <douglashernandezdura@gmail.com>
2022-06-07 21:00:04 -0600

    Agregando form devolucion y prestamo

--- devoluciones.php
+++ devoluciones.php
@@ -1,4 +1,5 @@
 <?php
-$title = 'Sistema Bibliotecario';
+$title = 'Devoluciones';
 require_once('php/components/head.php');
+require_once('php/view/devoluciones/index.php');
 require_once('php/components/footer.php');
--- prestamos.php
+++ prestamos.php
@@ -1,4 +1,5 @@
 <?php
-$title = 'Sistema Bibliotecario';
+$title = 'Prestamos';
 require_once('php/components/head.php');
+require_once('php/view/prestamos/index.php');
 require_once('php/components/footer.php');
