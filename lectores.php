3dba29a27d818953f78cb2b96a6a9c715e54b346
Douglas Hernandez <douglashernandezdura@gmail.com>
2022-06-09 16:33:30 -0600

    Index devolucion

---
+++ lectores.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Lectores';
+require_once('php/autoloadPrestamos.php');
+require_once('php/components/head.php');
+require_once('php/view/lectores/index.php');
+require_once('php/components/footer.php');
